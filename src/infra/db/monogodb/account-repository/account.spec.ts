9fdd6709e335c8f8a9ae510de321c5a184e56b7f
Henrique <hiquesc_@hotmail.com>
2020-06-18 19:52:27 -0300

    refactor: mongodb methods to a helper

---
+++ src/infra/db/monogodb/account-repository/account.spec.ts
@@ -0,0 +1,15 @@
+import { MongoHelper } from '../helper/mongo-helper'
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should return an account on success', async () => {
+
+  })
+})
