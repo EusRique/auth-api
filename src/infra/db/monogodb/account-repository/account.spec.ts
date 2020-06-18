9cd6a1c1f6a28459cd1fcd8445418b0352628d8b
Henrique <hiquesc_@hotmail.com>
2020-06-18 20:33:44 -0300

    test: ensure AccountMongoRepository returns an account on success

--- src/infra/db/monogodb/account-repository/account.spec.ts
+++ src/infra/db/monogodb/account-repository/account.spec.ts
@@ -1,4 +1,5 @@
 import { MongoHelper } from '../helper/mongo-helper'
+import { AccountMongoRepository } from './account'
 
 describe('Account Mongo Repository', () => {
   beforeAll(async () => {
@@ -10,6 +11,16 @@ describe('Account Mongo Repository', () => {
   })
 
   test('Should return an account on success', async () => {
-
+    const sut = new AccountMongoRepository()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@mail.com')
+    expect(account.password).toBe('any_password')
   })
 })
