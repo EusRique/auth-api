0782c0f25a3b979b89a5664bd3dbd321108e6277
Henrique <hiquesc_@hotmail.com>
2020-07-27 21:23:02 -0300

    test: ensure DbAuthentication returns null if LoadAccountByEmailRepository returns null

--- src/data/usecases/authentication/db-authetication.spec.ts
+++ src/data/usecases/authentication/db-authetication.spec.ts
@@ -52,4 +52,11 @@ describe('Db Authentication UseCase', () => {
     const promise = sut.auth(makeFakeAutentication())
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return null if LoadAccountByEmailRepository returns null', async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
+    jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValueOnce(null)
+    const accessToken = await sut.auth(makeFakeAutentication())
+    expect(accessToken).toBeNull()
+  })
 })
