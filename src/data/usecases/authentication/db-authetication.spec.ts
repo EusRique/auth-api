6b5148acd83934a9f5032d4ad9bf706c58c8d9fc
Henrique <hiquesc_@hotmail.com>
2020-07-27 22:47:10 -0300

    test: ensure DbAuthentication throws if TokenGenerator throws

--- src/data/usecases/authentication/db-authetication.spec.ts
+++ src/data/usecases/authentication/db-authetication.spec.ts
@@ -117,4 +117,11 @@ describe('Db Authentication UseCase', () => {
     await sut.auth(makeFakeAccount())
     expect(generateSpy).toHaveBeenCalledWith('any_id')
   })
+
+  test('Should throw if TokenGenerator throws', async () => {
+    const { sut, tokenGeneratorStub } = makeSut()
+    jest.spyOn(tokenGeneratorStub, 'generate').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.auth(makeFakeAutentication())
+    await expect(promise).rejects.toThrow()
+  })
 })
