f2ea75ca90b23c6179839de0e90074ea042be282
Henrique <hiquesc_@hotmail.com>
2021-02-26 21:06:42 -0300

    feat: add docker and docker-compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/authentication-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/authentication-api',
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || 'tj670==5H'
 }
