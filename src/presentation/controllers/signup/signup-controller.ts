60a574863d6acf410f3eca6084eea6dfcd6925dc
Henrique <hiquesc_@hotmail.com>
2021-03-20 16:29:40 -0300

    feat: return access token login and refactor separate factories in folders

--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -1,11 +1,13 @@
-import { HttpRequest, HttpResponse, Controller, AddAccount } from './signup-controller-protocols'
-import { badRequest, serverError, ok } from '../../helpers/http/http-helper'
+import { HttpRequest, HttpResponse, Controller, AddAccount, Authentication } from './signup-controller-protocols'
+import { badRequest, serverError, ok, forbidden } from '../../helpers/http/http-helper'
 import { Validation } from '../../protocols/validation'
+import { EmailInUseError } from '../../errors'
 
 export class SignUpController implements Controller {
   constructor (
     private readonly addAccount: AddAccount,
-    private readonly validation: Validation
+    private readonly validation: Validation,
+    private readonly authentication: Authentication
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -21,7 +23,16 @@ export class SignUpController implements Controller {
         password
       })
 
-      return ok(account)
+      if (!account) {
+        return forbidden(new EmailInUseError())
+      }
+
+      const accessToken = await this.authentication.auth({
+        email,
+        password
+      })
+
+      return ok({ accessToken })
     } catch (error) {
       return serverError(error)
     }
